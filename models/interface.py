fc242f63e3bbcc42c8fa82b9a9997e6e121189d0
Salako Oluwadolapo <willdynamics@gmail.com>
2023-08-25 03:38:53 -0500

    Add files via upload

---
+++ models/interface.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import os
+import tfci
+import subprocess
+
+def compress_image(image_path, algorithm, quality):
+    if algorithm == "Factorized Prior Autoencoder":
+        subprocess.run(["python", "tfci.py", "compress", f"bmshj2018-factorized-msssim-{quality}", image_path])
+        return image_path + ".tfci"
+    elif algorithm == "Nonlinear transform coder model with factorized priors":
+        subprocess.run(["python", "tfci.py", "compress", "b2018-gdn-128-1", image_path])
+        return image_path + ".tfci"
+    # Add logic for the third algorithm
+
+def decompress_image(image_path, algorithm):
+    decompressed_path = image_path.replace(".tfci", "_decompressed.jpg")
+    subprocess.run(["python", "tfci.py", "decompress", image_path, decompressed_path])
+    return decompressed_path
+
+def main():
+    st.title("Image Compression & Decompression App")
+
+    operation = st.selectbox("Choose Operation", ["Compress", "Decompress"])
+
+    uploaded_image = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
+    
+    if uploaded_image:
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+        
+        algorithm = st.selectbox("Choose Compression Algorithm", [
+            "Factorized Prior Autoencoder", 
+            "Nonlinear Transform Coder Model with Factorized Priors", 
+            "Hyperprior Model with Non Zero-Mean Gaussian Conditionals"
+        ])
+
+        if algorithm == "Factorized Prior Autoencoder":
+            quality = st.selectbox("Select a quality level", [1, 2, 3, 4, 5, 6, 7, 8])
+        elif algorithm == "Nonlinear Transform Coder Model with Factorized Priors":
+            quality = st.selectbox("Select a quality level", [1, 2, 3, 4])
+        elif algorithm == "Hyperprior Model with Non Zero-Mean Gaussian Conditionals":
+            quality = st.selectbox("Select a quality level", [1, 2, 3, 4, 5, 6, 7, 8])
+        if st.button("Execute"):
+            if operation == "Compress":
+                # Call the function to compress
+                compressed_file = compress_image(uploaded_image, algorithm, quality)
+                st.write("Compression Done!")
+                st.download_button(label="Download Compressed File", data=compressed_file)
+
+            else:
+                # Call the function to decompress
+                decompressed_file = decompress_image(uploaded_image, algorithm)
+                st.write("Decompression Done!")
+                st.download_button(label="Download Decompressed File", data=decompressed_file)
+
+if __name__ == "__main__":
+    main()
